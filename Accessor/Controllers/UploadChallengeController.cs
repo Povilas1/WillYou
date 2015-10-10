2342f584342e4305bc2428391de41cd2a8daba6d
Povilas1 <povilas.arl@gmail.com>
2015-10-10 16:17:30 +0300

    Posting

---
+++ Accessor/Controllers/UploadChallengeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using System.Configuration;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Accessor.Models
+    {
+    public class UploadChallengeController : ApiController
+    {
+        public  void Post()
+        {
+            string json = Request.Content.ReadAsStringAsync().Result;
+            Challenge challenge = DeserializeChallenges(json); 
+            //do smth with this challenge
+        }
+
+        private Challenge DeserializeChallenges(string json)
+        {
+            return JsonConvert.DeserializeObject<Challenge>(json);
+        }
+    }
+}
