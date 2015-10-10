d59beaef0c07a392614eed5a4a476deac2c38752
vstonkus <visvaldas.stonkus@gmail.com>
2015-10-10 16:57:40 +0300

    Storage provider

---
+++ Accessor/Controllers/RepositoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace Accessor.Controllers
+    {
+    public class RepositoryController : ApiController
+        {
+        public HttpResponseMessage Get()
+            {
+
+
+            //var json = JsonConvert.SerializeObject ();
+
+            return new HttpResponseMessage ()
+            {
+                Content = new StringContent (json)
+            };
+            }
+
+        }
+    }
---
+++ Accessor/StorageProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Accessor
+    {
+    public class StorageProvider
+        {
+        public static Lazy<CloudStorageAccount> StorageAccount = new Lazy<CloudStorageAccount> (() =>
+            {
+            var connectionString = ConfigurationManager.AppSettings["Storage.ConnectionString"];
+            return CloudStorageAccount.Parse (connectionString);
+            });
+
+        public static CloudTableClient GetTableClient()
+            {
+            return StorageAccount.Value.CreateCloudTableClient();
+            }
+
+        public static CloudBlobClient GetBlobClient()
+            {
+            return StorageAccount.Value.CreateCloudBlobClient();
+            }
+
+        public static CloudQueueClient GetQueueClient()
+            {
+            return StorageAccount.Value.CreateCloudQueueClient();
+            }
+        }
+    }
